4d8e8f02b569ad46b4ea02da4e606576e1ad215a
liuxiaodong <liuxiaodong>
2019-08-06 15:25:33 +0800

    add:新增加api测试用例

--- nsf-api-plane-server/src/test/java/com/netease/cloud/nsf/ApiConfigTest.java
+++ nsf-api-plane-server/src/test/java/com/netease/cloud/nsf/ApiConfigTest.java
@@ -1,17 +1,67 @@
 package com.netease.cloud.nsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.netease.cloud.nsf.meta.APIModel;
+import com.netease.cloud.nsf.meta.ApiOption;
+import com.netease.cloud.nsf.service.GatewayService;
 
 /**
  * Created by caukie on 2019/8/5.
  */
 @RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApiConfigTest {
+    private static Logger logger = LoggerFactory.getLogger(ApiConfigTest.class);
+
+    @Autowired
+    private GatewayService gatewayService;
+
     @Test
-    public void baseTest() {
+    public void baseTest() throws Exception {
         System.out.println("功能测试环境");
+        List<String> gateways = new ArrayList<>(3);
+        gateways.add("demo-gateway");
+
+        List<String> hosts = new ArrayList<>(3);
+        gateways.add("api-demo.com");
+
+        List<String> reqUris = new ArrayList<>(6);
+        reqUris.add("/");
+        reqUris.add("/status");
+
+        List<String> methods = new ArrayList<>(3);
+        methods.add("GET");
+
+        List<String> proxyUris = new ArrayList<>();
+        proxyUris.add("http://prxoy");
+
+        ApiOption apiOption = new ApiOption();
+
+
+        APIModel apiModel = APIModel.APIModelBuilder.anAPIModel()
+                .withGateways(gateways)
+                .withName("demo")
+                .withHosts(hosts)
+                .withRequestUris(reqUris)
+                .withMethods(methods)
+                .withProxyUris(proxyUris)
+                .withService("demo-service")
+                .withOption(apiOption)
+                .build();
+
+        gatewayService.updateAPI(apiModel);
+
+        // gatewayService.deleteAPI();
     }
 
     /**
