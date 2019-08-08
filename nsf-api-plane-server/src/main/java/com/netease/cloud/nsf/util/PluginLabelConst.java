936f5d2b0039fa10793bb1f7cae87544e1e8f71b
wupenghuai <wupenghuai@corp.netease.com>
2019-08-08 11:38:48 +0800

    Enhance: TemplateUtils优化

---
+++ nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/util/PluginLabelConst.java
@@ -0,0 +1,17 @@
+package com.netease.cloud.nsf.util;
+
+/**
+ * @auther wupenghuai@corp.netease.com
+ * @date 2019/8/7
+ **/
+public class PluginLabelConst {
+    public static final String LABEL_DESCRIPTION = "description";
+    public static final String LABEL_TYPE = "type";
+    public static final String LABEL_INNER_TYPE = "innerType";
+    public static final String LABEL_VERSION = "version";
+    public static final String LABEL_PROCESSOR = "processor";
+
+    public static final String VALUE_PLUGIN_SCHEMA = "pluginTemplate";
+    public static final String VALUE_ISTIO_FRAGMENT = "istioSchema";
+    public static final String VALUE_INNER_ISTIO_FRAGMENT = "innerIstioSchema";
+}
