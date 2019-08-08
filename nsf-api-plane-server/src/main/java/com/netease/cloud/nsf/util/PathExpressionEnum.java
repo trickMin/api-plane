f0f78f7a006d4f7e3d18f921ebcc09eb0f565028
zhranklin <chigou79@outlook.com>
2019-08-08 10:12:18 +0800

    白名单操作clusterrbacconfig对象

--- nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/util/PathExpressionEnum.java
+++ nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/util/PathExpressionEnum.java
@@ -20,6 +20,7 @@ public enum PathExpressionEnum {
     GET_ITEMS("$.items", 0),
     REMOVE_RBAC_SERVICE("$.spec.rules[?]", 0),
     ADD_RBAC_SERVICE("$.spec.rules", 0),
+    ADD_RBAC_SERVICE_TO_RBAC_CONFIG("$.spec.inclusion.services", 0),
     REMOVE_VS_HTTP("$.spec.http[?(@.name == '%s')]", 1),
     REMOVE_DST_SUBSET("$.spec.subsets[?(@.name == '%s')]", 1),
 
