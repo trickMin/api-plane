23ccb1fd42525696aece89257f2d84c677e52fc5
wupenghuai <wupenghuai@corp.netease.com>
2019-08-21 11:41:23 +0800

    Enhance: 增加插件开发相关类注释

--- nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/core/plugin/FragmentTypeEnum.java
+++ nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/core/plugin/FragmentTypeEnum.java
@@ -5,5 +5,8 @@ package com.netease.cloud.nsf.core.plugin;
  * @date 2019/8/20
  **/
 public enum FragmentTypeEnum {
-    DEFAULT_MATCH, NEW_MATCH;
+    // GatewayModelProcessor会将Fragment合并VirtualService的一个默认match中
+    DEFAULT_MATCH,
+    // GatewayModelProcessor会将Fragment作为新match创建到VirtualService中
+    NEW_MATCH,
 }
