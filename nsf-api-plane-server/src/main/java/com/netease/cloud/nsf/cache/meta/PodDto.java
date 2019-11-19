fefa1059a1f4cdc860fd3e1ffd73d2e02e7b561d
zhangzihao <zhangzihao1@corp.netease.com>
2019-11-19 11:38:55 +0800

    Enhance:#CLDNSF-4254 poddto增加资源信息

--- nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/cache/meta/PodDto.java
+++ nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/cache/meta/PodDto.java
@@ -1,16 +1,42 @@
 package com.netease.cloud.nsf.cache.meta;
 
 import io.fabric8.kubernetes.api.model.*;
+import org.springframework.util.StringUtils;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * @author zhangzihao
+ */
 public class PodDto<T extends HasMetadata> extends K8sResourceDto {
+
+    private static final String RESOURCE_VALUE_PATTERN = "[\\d]+(?=\\D*)";
+    private static final String RESOURCE_UNIT_PATTERN = "(?<=\\d)\\D*";
 
     private String hostIp;
 
     private String podIp;
 
+    private String status;
+
+    private String totalLimitCpu;
+
+    private String totalLimitMemory;
+
+    private int totalRestartCount;
+
+
     private List<ContainerInfo> containerInfoList = new ArrayList<>();
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
     public String getHostIp() {
         return hostIp;
@@ -34,6 +60,7 @@ public class PodDto<T extends HasMetadata> extends K8sResourceDto {
             Pod pod = (Pod) obj;
             this.hostIp = pod.getStatus().getHostIP();
             this.podIp = pod.getStatus().getPodIP();
+            this.status = pod.getStatus().getPhase();
             Map<String, ContainerInfo> containerInfoMap = new HashMap<>();
             // 更新容器资源信息
             pod.getSpec().getContainers().forEach(c -> {
@@ -45,8 +72,8 @@ public class PodDto<T extends HasMetadata> extends K8sResourceDto {
             // 更新容器状态信息
             pod.getStatus().getContainerStatuses().forEach(cs -> {
                 Objects.requireNonNull(containerInfoMap.computeIfAbsent(cs.getName(), ContainerInfo::new))
-                        .setStatusInfo("status", pod.getStatus().getPhase())
                         .setStatusInfo("restartCount", cs.getRestartCount().toString());
+                totalRestartCount += cs.getRestartCount();
             });
 
             containerInfoList.addAll(containerInfoMap.values());
@@ -86,11 +113,62 @@ public class PodDto<T extends HasMetadata> extends K8sResourceDto {
             if (resourceMap == null || resourceMap.isEmpty()) {
                 return this;
             }
+            float totalLimitCpuValue = 0;
+            float totalLimitMemoryValue = 0;
             for (Map.Entry<String, Quantity> entry : resourceMap.entrySet()) {
                 resourceLimit.put(entry.getKey(), entry.getValue().getAmount());
+
+                float limitCpuValue = getResourceValue(resourceMap.get("cpu"));
+                String limitCpuUnit = getResourceUnit(resourceMap.get("cpu"));
+                if (limitCpuUnit.contains("m")) {
+                    limitCpuValue = limitCpuValue / 1000;
+                }
+                float limitMemoryValue = getResourceValue(resourceMap.get("memory"));
+                String limitMemoryUnit = getResourceUnit(resourceMap.get("memory"));
+                if (limitMemoryUnit.contains("G")) {
+                    limitMemoryValue = limitMemoryValue * 1024;
+                }
+                totalLimitCpuValue += limitCpuValue;
+                totalLimitMemoryValue += limitMemoryValue;
             }
+            PodDto.this.totalLimitCpu = (totalLimitCpuValue == 0) ? null : totalLimitCpuValue + " Cores";
+            PodDto.this.totalLimitMemory = (totalLimitMemoryValue == 0) ? null : totalLimitMemoryValue + " MiB";
             return this;
         }
+    }
+
+
+    private int getResourceValue(Quantity quantity) {
+        if (quantity == null) {
+            return 0;
+        }
+        String amount = quantity.getAmount();
+        if (StringUtils.isEmpty(amount)) {
+            return 0;
+        }
+        Pattern p = Pattern.compile(RESOURCE_VALUE_PATTERN);
+        Matcher matcher = p.matcher(amount);
+        if (matcher.find()) {
+            String value = matcher.group();
+            return Integer.parseInt(value);
+        }
+        return 0;
+    }
+
+    private String getResourceUnit(Quantity quantity) {
+        if (quantity == null) {
+            return "";
+        }
+        String amount = quantity.getAmount();
+        if (StringUtils.isEmpty(amount)) {
+            return "";
+        }
+        Pattern p = Pattern.compile(RESOURCE_UNIT_PATTERN);
+        Matcher matcher = p.matcher(amount);
+        if (matcher.find()) {
+            return matcher.group();
+        }
+        return "";
 
 
     }
