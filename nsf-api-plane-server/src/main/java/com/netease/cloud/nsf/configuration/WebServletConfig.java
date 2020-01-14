ccd3850bd9983dda02faf6d74d5ee6e7a4389c3c
陈佳翰 <chenjiahan@corp.netease.com>
2020-01-14 15:09:56 +0800

    Enhance: 增加全局Request,Response日志

--- nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/configuration/WebServletConfig.java
+++ nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/configuration/WebServletConfig.java
@@ -1,10 +1,13 @@
 package com.netease.cloud.nsf.configuration;
 
+import com.netease.cloud.nsf.web.filter.CacheHttpRequestFilter;
 import com.netease.cloud.nsf.web.filter.LogUUIDFilter;
 import com.netease.cloud.nsf.web.filter.RequestContextHolderFilter;
+import com.netease.cloud.nsf.web.interceptor.RequestLogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -30,4 +33,19 @@ public class WebServletConfig extends WebMvcConfigurerAdapter {
 		return registration;
 	}
 
+	@Bean
+	public FilterRegistrationBean cacheHttpRequestFilter() {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(new CacheHttpRequestFilter());
+		registration.addUrlPatterns("/*");
+		registration.setName(CacheHttpRequestFilter.class.getSimpleName());
+		registration.setOrder(1);
+		return registration;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new RequestLogInterceptor()).addPathPatterns("/**");
+		super.addInterceptors(registry);
+	}
 }
