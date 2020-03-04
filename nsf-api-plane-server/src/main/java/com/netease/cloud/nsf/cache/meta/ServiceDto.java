17251a982a40af24019432bc027b1da2b44dcbe2
zhangzihao <zhangzihao1@corp.netease.com>
2020-03-04 23:45:31 +0800

    Enhance:k8s service列表优化

--- nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/cache/meta/ServiceDto.java
+++ nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/cache/meta/ServiceDto.java
@@ -2,7 +2,11 @@ package com.netease.cloud.nsf.cache.meta;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServicePort;
+import org.springframework.util.CollectionUtils;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ServiceDto<T extends HasMetadata> extends K8sResourceDTO {
@@ -13,12 +17,45 @@ public class ServiceDto<T extends HasMetadata> extends K8sResourceDTO {
 
     private String appName;
 
+    private String serviceType;
+
+    private List<String> serviceAddress;
+
 
     public ServiceDto(T obj, String clusterId) {
         super(obj,clusterId);
         Service service = (Service)obj;
         this.labels = service.getMetadata().getLabels();
         this.selectLabels = service.getSpec().getSelector();
+        this.serviceType = service.getSpec().getType();
+        String address = service.getMetadata().getName() + "." + this.getNamespace();
+        List<String> serviceAddressList = new ArrayList<>();
+
+        List<ServicePort> ports = service.getSpec().getPorts();
+        if (CollectionUtils.isEmpty(ports)){
+            serviceAddressList.add(address);
+        }else {
+            ports.forEach(p->{
+                serviceAddressList.add(address + ":"+p.getPort());
+            });
+        }
+        this.serviceAddress = serviceAddressList;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public List<String> getServiceAddress() {
+        return serviceAddress;
+    }
+
+    public void setServiceAddress(List<String> serviceAddress) {
+        this.serviceAddress = serviceAddress;
     }
 
     public String getAppName() {
