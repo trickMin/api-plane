385a2655ccd3540bc8afdc3ff51847b8cf77fe3c
陈佳翰 <chenjiahan@corp.netease.com>
2020-03-18 16:10:08 +0800

    Fix: 修复单测grpc端口冲突问题,修复单测unknownhost问题

--- nsf-api-plane-server/src/test/java/com/netease/cloud/nsf/core/BaseTest.java
+++ nsf-api-plane-server/src/test/java/com/netease/cloud/nsf/core/BaseTest.java
@@ -4,15 +4,15 @@ import com.netease.cloud.nsf.ApiPlaneApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @Author chenjiahan | chenjiahan@corp.netease.com | 2019/8/26
  **/
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ApiPlaneApplication.class)
-@PropertySource("classpath:application.properties")
+@SpringBootTest(classes = ApiPlaneApplication.class, properties = {"k8s.clusters.default.k8s-api-server=https://1.1.1.1"})
+@Import(BaseConfiguration.class)
 public class BaseTest {
 
     @Test
