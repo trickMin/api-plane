acdd5c8fe42bd3e416a0b088815f247157794a5b
张子豪 <zhangzihao1@corp.netease.com>
2020-09-14 10:39:48 +0800

    Enhance: 如果Service label中指定了app标签，则以此label的value作为app的值

--- nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/cache/K8sResourceCache.java
+++ nsf-api-plane-server/src/main/java/com/netease/cloud/nsf/cache/K8sResourceCache.java
@@ -245,6 +245,10 @@ public class K8sResourceCache<T extends HasMetadata> implements ResourceCache {
                 continue;
             }
             String appName = k8sService.getSpec().getSelector().get(meshConfig.getSelectorAppKey());
+            if (k8sService.getMetadata().getLabels()!=null
+                    && !StringUtils.isEmpty(k8sService.getMetadata().getLabels().get(meshConfig.getAppKey()))){
+                appName = k8sService.getMetadata().getLabels().get(meshConfig.getAppKey());
+            }
             if (projectId.equals(extractor.getResourceInfo(service, Const.RESOURCE_TARGET, projectId)) && serviceName.equals(appName)) {
                 String key = appName
                         + Const.SEPARATOR_DOT
@@ -404,6 +408,10 @@ public class K8sResourceCache<T extends HasMetadata> implements ResourceCache {
                 continue;
             }
             String appName = k8sService.getSpec().getSelector().get(meshConfig.getSelectorAppKey());
+            if (k8sService.getMetadata().getLabels()!=null
+                    && !StringUtils.isEmpty(k8sService.getMetadata().getLabels().get(meshConfig.getAppKey()))){
+                appName = k8sService.getMetadata().getLabels().get(meshConfig.getAppKey());
+            }
             String key = appName
                     + Const.SEPARATOR_DOT
                     + k8sService.getMetadata().getNamespace();
@@ -699,7 +707,12 @@ public class K8sResourceCache<T extends HasMetadata> implements ResourceCache {
                 .map(s->{
                     ServiceDto<T> tServiceDto = new ServiceDto<>(s, clusterId);
                     if (tServiceDto.getSelectLabels() != null){
-                        tServiceDto.setAppName(tServiceDto.getSelectLabels().get(meshConfig.getSelectorAppKey()));
+                        String appName = tServiceDto.getSelectLabels().get(meshConfig.getSelectorAppKey());
+                        if (tServiceDto.getLabels()!=null
+                                && !StringUtils.isEmpty(tServiceDto.getLabels().get(meshConfig.getAppKey()))){
+                            appName = tServiceDto.getLabels().get(meshConfig.getAppKey());
+                        }
+                        tServiceDto.setAppName(appName);
 
                     }
                     return tServiceDto;
