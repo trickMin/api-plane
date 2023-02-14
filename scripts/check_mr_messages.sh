102725446eb3f20f1508ca6c49316c2341a2b1a1
hanjiahao <hanjiahao@corp.netease.com>
2023-02-14 21:08:20 +0800

    Task-91032: add check_sonar.sh for api-plane
    Signed-off-by: hanjiahao <hanjiahao@corp.netease.com>

--- scripts/check_mr_messages.sh
+++ scripts/check_mr_messages.sh
@@ -1,5 +1,3 @@
-PROJECT=skiff-api-plane-23
-COMMIT="$(git rev-parse HEAD)"
 SB=$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
 TB=$CI_MERGE_REQUEST_TARGET_BRANCH_NAME
 git checkout $SB
@@ -22,33 +20,3 @@ if [[ $INVALID_COMMITS != "" ]]; then
   exit 1
 fi
 exit 0
-
-wget -O yq https://github.com/mikefarah/yq/releases/download/v4.30.6/yq_linux_amd64
-chmod +x yq
-
-echo Sonar link: "https://sonar-hy.netease.com/project/issues?id=$PROJECT&pullRequest=$COMMIT"
-TIMES=15
-for i in `seq 1 $TIMES`; do
-  RESP="$(curl -u 4c70c4b1e65ba13757239d6fea4cd5e548ad3e22: "https://sonar-hy.netease.com/api/measures/component?pullRequest=$COMMIT&component=$PROJECT&metricKeys=new_blocker_violations,new_critical_violations,new_major_violations")"
-  STATS="$(echo "$RESP" | ./yq -P '.component.measures[]|{.metric: .period.value}')"
-  echo "$RESP"
-  echo "$STATS"
-
-  BLOCKER=$(echo "$STATS" | ./yq '.new_blocker_violations')
-  CRITICAL=$(echo "$STATS" | ./yq '.new_critical_violations')
-
-  if [[ $BLOCKER != 0 || $CRITICAL != 0 ]]; then
-    if echo "$RESP" | ./yq -P '.errors[0].msg' | grep -q "not found"; then
-      echo sonar result not found, retrying...
-      sleep 60
-    else
-      echo ERROR: There is new issue.
-      exit 1
-    fi
-  else
-    exit 0
-  fi
-done
-
-echo sonar result not found, after $TIMES tries...
-exit 1
